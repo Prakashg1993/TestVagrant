f34678505f8d53f33c2f103ed266b49e8480b120
prakashg1993 <prakash.qasdet@gmail.com>
2020-09-15 17:08:27 +0000

    Commit

---
+++ src/main/java/listeners/testListeners.java
@@ -0,0 +1,49 @@
+package listeners;
+
+import org.testng.ITestListener;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+
+public class testListeners implements ITestListener{
+	
+	public void test() {
+		
+	}
+
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ target/test-classes/features/webAutomate.feature
@@ -0,0 +1,4 @@
+@tag1
+Feature: Web Automate NDtv Weather Report
+
+Scenario: Get the details
